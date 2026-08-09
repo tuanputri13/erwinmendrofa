94f265fce4d299b290865db6c408c305c3395cc6
tuanputri13 <erwin351@guru.smk.belajar.id>
2026-08-09 22:31:06 +0700

    fix: configure tailwind css

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id" className="scroll-smooth">
-      <body className="bg-slate-900 text-slate-100 antialiased">
+      <body className="bg-slate-950 text-slate-100 antialiased">
         {children}
       </body>
     </html>
